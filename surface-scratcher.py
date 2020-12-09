8c4805ac737769eb3c7d4ac28c29766b1ddc0dea
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2020-12-09 16:01:53 +0000

    Specify downloader interface

--- surface-scratcher.py
+++ surface-scratcher.py
@@ -1,6 +1,21 @@
 from bs4 import BeautifulSoup as bs
-import requests
 import urllib.request
+import argparse
+import requests
+import os
+
+def get_args():
+    def to_download(arg):
+        if arg == "all" or arg.isnumeric():
+            return arg
+        raise argparse.ArgumentTypeError("invalid expression for download flag. Please specify an integer or write 'all'")
+    parser = argparse.ArgumentParser(description = "Download images and labels from scratchthesurface.tumblr.com")
+    parser.add_argument("-n", "--no_context", help = "(optional) - disables the download of context images", action = "store_true")
+    parser.add_argument("-u", "--update", help = "(optional) - updates the local dataset with newly added surfaces", action = "store_true")
+    parser.add_argument("-f", "--full_update", help = "(optional) - checks and updates any difference of local dataset with the remote dataset", action = "store_true")
+    parser.add_argument("-d", "--download", help = "(optional) - downloads the surface information of the specified surface index", type = to_download)
+    parser.add_argument("-v", "--verbose", help = "(optional) - details what the program is doing", action = "store_true")
+    return parser.parse_args()
 
 
 def do_nothing(soup):
@@ -17,14 +32,23 @@ def iter_posts(post_url, post_processor = do_nothing):
         iter_posts(post_url, post_processor)
         
 def image_scratcher(soup, scratch_context = True):
-    img = soup.find("img", {"class": "notPhotoset"})
-    img_url = img["src"]
-    name = img_url.split("/")[-1]
-    urllib.request.urlretrieve(img_url, name)
+    # img = soup.find("img", {"class": "notPhotoset"})
+    # img_url = img["src"]
+    # name = img_url.split("/")[-1]
+    # urllib.request.urlretrieve(img_url, name)
+    if scratch_context:
+        context = soup.find("figure", {"class": "tmblr-full"}).find("img")
+        print(context["src"])
 
 
 if __name__ == "__main__":
-    post_url = "https://scratchthesurface.tumblr.com/post/635159455418466304/scratch-the-surface-1-18nov2020-coarse"
-    iter_posts(post_url, image_scratcher)
+    args = get_args()
+    print(args)
+    # if not os.path.isdir("surfaces"):
+    #     os.mkdir("surfaces")
+    # if not os.path.isdir("context"):
+    #     os.mkdir("context")
+    # post_url = "https://scratchthesurface.tumblr.com/post/635159455418466304/scratch-the-surface-1-18nov2020-coarse"
+    # iter_posts(post_url, image_scratcher)
 
     
